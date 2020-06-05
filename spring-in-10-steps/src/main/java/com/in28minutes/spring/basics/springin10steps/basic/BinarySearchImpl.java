33646f7abdf844aa857525e2d71c967335e1bf52
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-05 16:21:59 +0200

    LifeCycle of a bean

--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/basic/BinarySearchImpl.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package com.in28minutes.spring.basics.springin10steps.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
@@ -8,6 +13,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class BinarySearchImpl {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	@Qualifier("quick")
@@ -25,5 +32,15 @@ public class BinarySearchImpl {
 		// Search the array
 
 		return 3;
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 }
