fe4d835737eb922b0501c4a8db2862b0d408b55d
Arsene Kevin <kouomeukevin@gmail.com>
2020-06-05 13:29:51 +0200

    Update BinarySearchImpl.java, SpringIn10StepsApplication.java, and log.txt Scope of a Bean Prototype and Singleton

--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -2,9 +2,11 @@ package com.in28minutes.spring.basics.springin10steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BinarySearchImpl {
 
 	@Autowired
--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
@@ -11,7 +11,13 @@ public class SpringIn10StepsApplication {
 
 		// APPLICATION CONTEXT --> Manager of BEANS
 		ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class, args);
+
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
+
 		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 
 		System.out.println(result);
