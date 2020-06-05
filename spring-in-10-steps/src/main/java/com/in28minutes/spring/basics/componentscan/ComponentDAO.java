d2061d76014e5d4c16d3edb70c0ef3affae55a51
Arsene Kevin <kouomeukevin@gmail.com>
2020-06-05 14:52:45 +0200

    Using the component scan to scan for beans (outside of the package which run the spring application)

---
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/componentscan/ComponentDAO.java
@@ -0,0 +1,23 @@
+package com.in28minutes.spring.basics.componentscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class ComponentDAO {
+
+	@Autowired
+	ComponentJdbcConnection componentJdbcConnection;
+
+	public ComponentJdbcConnection getComponentJdbcConnection() {
+		return componentJdbcConnection;
+	}
+
+	public void setComponentJdbcConnection(ComponentJdbcConnection componentJdbcConnection) {
+		this.componentJdbcConnection = componentJdbcConnection;
+	}
+
+}
---
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsComponentScanApplication.java
@@ -0,0 +1,33 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.in28minutes.spring.basics.componentscan.ComponentDAO;
+
+/**
+ * ComponentScan is useful to scan for a beans outside the package of
+ * SpringBootApplication
+ */
+
+@SpringBootApplication
+@ComponentScan("com.in28minutes.spring.basics.componentscan")
+public class SpringIn10StepsComponentScanApplication {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn10StepsComponentScanApplication.class);
+
+	public static void main(String[] args) {
+
+		// APPLICATION CONTEXT --> Manager of BEANS
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsComponentScanApplication.class,
+				args);
+
+		ComponentDAO componentDao = applicationContext.getBean(ComponentDAO.class);
+
+		LOGGER.info("{}", componentDao);
+	}
+}
