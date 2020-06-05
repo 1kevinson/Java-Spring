d9b81ec4d702e61fd43e6d5b15cd4080b5e09479
Arsene Kevin <kouomeukevin@gmail.com>
2020-06-05 12:46:22 +0200

    Update BinarySearchImpl.java, BubbleSortAlgorithm.java, and 3 more files... Autowiring Qualifier annotation

--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -1,16 +1,20 @@
 package com.in28minutes.spring.basics.springin10steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
 	@Autowired
+	@Qualifier("quick")
 	private SortAlgorithm sortAlgorithm;
 
 	// AutoWiring by name -> no need to use @Primary
 	// private SortAlgorithm sortAlgorithm;
+
+	// AutoWiring with Qualifier is better when we have most of beans
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 
--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BubbleSortAlgorithm.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/BubbleSortAlgorithm.java
@@ -1,10 +1,10 @@
 package com.in28minutes.spring.basics.springin10steps;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Qualifier("bubble")
 public class BubbleSortAlgorithm implements SortAlgorithm {
 
 	public int[] sort(int[] numbers) {
