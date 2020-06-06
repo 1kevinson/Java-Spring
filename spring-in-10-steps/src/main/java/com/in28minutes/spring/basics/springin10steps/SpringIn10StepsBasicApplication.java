e4505db1ef688b7bb303a8a9f1e7e8e7762643f3
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-06 10:32:11 +0200

    Removing spring boot in basic application

--- spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsBasicApplication.java
+++ spring-in-10-steps/src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsBasicApplication.java
@@ -1,18 +1,21 @@
 package com.in28minutes.spring.basics.springin10steps;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.springin10steps.basic.BinarySearchImpl;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringIn10StepsBasicApplication {
 
 	public static void main(String[] args) {
 
 		// APPLICATION CONTEXT --> Manager of BEANS
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsBasicApplication.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				SpringIn10StepsBasicApplication.class);
 
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
