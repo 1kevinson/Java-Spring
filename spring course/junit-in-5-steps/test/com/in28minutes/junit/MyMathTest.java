54d78032c95299c4227dbfafcdd7d44399bc82f5
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-07 10:38:30 +0200

    First unit test

--- spring course/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
+++ spring course/junit-in-5-steps/test/com/in28minutes/junit/MyMathTest.java
@@ -1,17 +1,23 @@
 package com.in28minutes.junit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class MyMathTest {
 
+	MyMath myMath = new MyMath();
+
 	@Test
-	public void test() {
-		MyMath myMath = new MyMath();
-		int sum = myMath.sum(new int[] { 3, 5, 9, 6, 1 });
+	public void sumWith4numbers() {
 
-		// Check that the result is 24
+		assertEquals(19, myMath.sum(new int[] { 3, 9, 6, 1 }));
+	}
 
-		System.out.println(sum);
+	@Test
+	public void sumWith3numbers() {
+
+		assertEquals(10, myMath.sum(new int[] { 3, 6, 1 }));
 	}
 
 }
