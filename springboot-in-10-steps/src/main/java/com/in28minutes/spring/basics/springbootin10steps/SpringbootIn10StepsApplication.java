e32a90e2093efc3cc82ab45d1f2158a5bc64dbf9
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-10 14:20:43 +0200

    Spring boot actuator

--- springboot-in-10-steps/src/main/java/com/in28minutes/spring/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/spring/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -1,10 +1,13 @@
 package com.in28minutes.spring.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
