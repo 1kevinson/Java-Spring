f697114728985b905a8e40b6de75d93eaf393008
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-10 18:34:20 +0200

    Execute the findAll method using CommandLineRunner

--- database-demo/src/main/java/com/in28minutes/database/databsedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/in28minutes/database/databsedemo/DatabaseDemoApplication.java
@@ -1,13 +1,32 @@
 package com.in28minutes.database.databsedemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.database.databsedemo.jdbc.PersonJdbcDAO;
+
+import net.bytebuddy.asm.Advice.This;
+
 @SpringBootApplication
-public class DatabaseDemoApplication {
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(This.class);
+
+	@Autowired
+	PersonJdbcDAO dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("All the users -> {}", dao.findAll());
 	}
 
 }
--- database-demo/src/main/java/com/in28minutes/database/databsedemo/entity/Person.java
+++ database-demo/src/main/java/com/in28minutes/database/databsedemo/entity/Person.java
@@ -1,5 +1,63 @@
 package com.in28minutes.database.databsedemo.entity;
 
+import java.util.Date;
+
 public class Person {
+
+	private int id;
+	private String name;
+	private String location;
+	private Date birthdate;
+
+	// Define the no argument construcstor because we use "BeanPropertyRowMapper"
+	public Person() {
+
+	}
+
+	public Person(int id, String name, String location, Date birthdate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+		this.birthdate = birthdate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	// Override this method to customize the sysout input
+	@Override
+	public String toString() {
+		return "\n Person [id=" + id + ", name=" + name + ", location=" + location + ", birthdate=" + birthdate + "]";
+	}
 
 }
