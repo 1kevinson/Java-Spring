d7dc01eab793f02a9ea8fd04d37d5ed06542687b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-10 14:25:02 +0200

    Spring Boot devtools

--- springboot-in-10-steps/src/main/java/com/in28minutes/spring/basics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/spring/basics/springbootin10steps/BooksController.java
@@ -11,7 +11,7 @@ public class BooksController {
 
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
-		return Arrays.asList(new Book(1l, "Master Spring Famework", "in28minutes"));
+		return Arrays.asList(new Book(1l, "Master Spring Famework 5.1", "in28minutes"));
 	}
 
 }
