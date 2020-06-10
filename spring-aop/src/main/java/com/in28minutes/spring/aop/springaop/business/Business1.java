f8f5cfd8b1bf6579750c732935d22e8aa7cf2c96
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-10 16:56:07 +0200

    Update SpringAopApplication.java, BeforeAspect.java, and 4 more files... -- Add logger

--- spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/business/Business1.java
+++ spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/business/Business1.java
@@ -1,18 +1,20 @@
-package com.in28minutes.spring.aop.springaop;
+package com.in28minutes.spring.aop.springaop.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.in28minutes.spring.aop.springaop.data.Dao1;
+
 @Service
-public class Business2 {
+public class Business1 {
 
 	@Autowired
-	private Dao2 dao2;
+	private Dao1 dao1;
 
 	public String calculateSomething() {
 
 		// Business Logic
-		return dao2.retrieveSomething();
+		return dao1.retrieveSomething();
 	}
 
 }
--- spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/business/Business2.java
+++ spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/business/Business2.java
@@ -1,18 +1,20 @@
-package com.in28minutes.spring.aop.springaop;
+package com.in28minutes.spring.aop.springaop.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.in28minutes.spring.aop.springaop.data.Dao2;
+
 @Service
-public class Business1 {
+public class Business2 {
 
 	@Autowired
-	private Dao1 dao1;
+	private Dao2 dao2;
 
 	public String calculateSomething() {
 
 		// Business Logic
-		return dao1.retrieveSomething();
+		return dao2.retrieveSomething();
 	}
 
 }
